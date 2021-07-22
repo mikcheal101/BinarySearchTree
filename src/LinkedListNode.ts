2068d10f12de02b789f92fd4f0d0d10a7339d7df
hirekaan.hemen <hirekaan.hemen@konga.com>
2021-07-22 03:01:06 +0100

    LinkedListNode defined

---
+++ src/LinkedListNode.ts
@@ -0,0 +1,11 @@
+class LinkedListNode {
+    public value: any;
+    public next: LinkedListNode | null;
+
+    constructor(value: any) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+export default LinkedListNode;
