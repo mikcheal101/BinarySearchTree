2ab792090218251b41b9c0930ce0c1104200e566
hirekaan.hemen <hirekaan.hemen@konga.com>
2021-07-22 03:13:14 +0100

    previous added to LinkedListNode

--- src/LinkedListNode.ts
+++ src/LinkedListNode.ts
@@ -1,10 +1,12 @@
 class LinkedListNode {
     public value: any;
     public next: LinkedListNode | null;
+    public previous: LinkedListNode | null;
 
     constructor(value: any) {
         this.value = value;
         this.next = null;
+        this.previous = null;
     }
 }
 
